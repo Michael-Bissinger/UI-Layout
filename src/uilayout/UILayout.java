cd4aa74a63b5013dd4108c2bd2da0482b1bbe2ad
Michael Bissinger <Michael.Bissinger@inb.de>
2019-02-22 09:34:58 +0100

    Add Passwordfield

--- src/uilayout/UILayout.java
+++ src/uilayout/UILayout.java
@@ -15,6 +15,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -39,6 +40,10 @@ public class UILayout extends Application {
         rb.setStyle("-fx-base: #61a2b1");
         CheckBox cb = new CheckBox();
         
+        PasswordField pw = new PasswordField();
+        
+        
+        
         //Pick Layout
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
@@ -53,7 +58,7 @@ public class UILayout extends Application {
         grid.add(rb,0,1);
         grid.add(cb,1,1);
         
-        
+        grid.add(pw,3,0);
                 
         
         
