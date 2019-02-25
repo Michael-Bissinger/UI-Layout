c7c06cbf7a08909f613e85c4f0ebed5c7f8f3a14
Michael Bissinger <Michael.Bissinger@inb.de>
2019-02-25 11:48:46 +0100

    Delete everything from the old layout

--- src/uilayout/UILayout.java
+++ src/uilayout/UILayout.java
@@ -31,41 +31,7 @@ public class UILayout extends Application {
     @Override
     public void start(Stage primaryStage) {
         
-        //Create the UI controls
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        Label lbl = new Label("I'm a label");
-        TextField tf = new TextField("Enter text here");
-        Hyperlink hl = new Hyperlink("I'm a hyperlink");
-        RadioButton rb = new RadioButton("RadioButton");
-        rb.setStyle("-fx-base: #61a2b1");
-        CheckBox cb = new CheckBox();
-        
-        PasswordField pw = new PasswordField();
-        
-        
-        //Needed for Helbling File Renamer
-        //FileChooser fc = new Filechooser();
-        //fc.setTitle("Pick your file");
-        //fc.showOpenDialog(primaryStage);
-        
-        
-        //Pick Layout
-        GridPane grid = new GridPane();
-        grid.setAlignment(Pos.CENTER);
-        grid.setHgap(20);
-        grid.setVgap(50);
-        grid.setPadding(new Insets(25,25,25,25));
-        
-        //add controls to Grid
-        grid.add(btn,0,0);
-        grid.add(lbl,1,0);
-        grid.add(tf,2,0);
-        grid.add(rb,0,1);
-        grid.add(cb,1,1);
-        
-        grid.add(pw,3,0);
-        
+ 
         
         
         btn.setOnAction(new EventHandler<ActionEvent>() {
@@ -83,7 +49,7 @@ public class UILayout extends Application {
         
         Scene scene = new Scene(grid, 300, 250);
         
-        primaryStage.setTitle("User controls!");
+        primaryStage.setTitle("UI control event handler!");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
