6bb58e8583476baad1959e053002d80889d1f997
Michael Bissinger <Michael.Bissinger@inb.de>
2019-02-28 11:01:05 +0100

    Clear project

--- src/uilayout/UILayout.java
+++ src/uilayout/UILayout.java
@@ -31,42 +31,7 @@ public class UILayout extends Application {
         BorderPane root = new BorderPane();        
         Scene scene = new Scene(root, 400, 400, Color.WHITE);
         
-        MenuBar menuBar = new MenuBar();
-        menuBar.prefWidthProperty().bind(primaryStage.widthProperty());
-        root.setTop(menuBar);
-        
-        //File menu - new save print exit
-        Menu fileMenu = new Menu();
-        MenuItem newMenuItem = new MenuItem("New");
-        MenuItem saveMenuItem = new MenuItem("Save");
-        MenuItem printMenuItem = new MenuItem("print");
-        MenuItem exitMenuItem = new MenuItem("Exit");
-        
-        //add the new menuitems to the filemenu
-        fileMenu.getItems().addAll(newMenuItem, saveMenuItem, printMenuItem, new SeparatorMenuItem(), exitMenuItem);
-        
-        Menu languageMenu = new Menu("Language");
-        CheckMenuItem javaMenuItem = new CheckMenuItem("Java"); // CheckMenuItem creates a checkable menu
-        CheckMenuItem pythonMenuItem = new CheckMenuItem("Python");
-        CheckMenuItem htmlMenuItem = new CheckMenuItem("HTML");
-                languageMenu.getItems().addAll(javaMenuItem, pythonMenuItem, htmlMenuItem); // Adding items to menu
-        
-        CheckMenuItem FXMenuItem = new CheckMenuItem("JavaFX");
-        FXMenuItem.setSelected(true);
-                languageMenu.getItems().addAll(new SeparatorMenuItem(),FXMenuItem); // Adding Items to menu
-        
-        Menu tutorialMenu = new Menu("Tutorial");
-        tutorialMenu.getItems().addAll(
-            new MenuItem("Buttons"),
-            new MenuItem("Menus"),
-            new MenuItem("Images"));
-    
-        languageMenu.getItems().add(tutorialMenu);
-        
-        exitMenuItem.setOnAction(actionEvent->Platform.exit());
-        
-        menuBar.getMenus().addAll(fileMenu, languageMenu);
-        
+       
         primaryStage.setTitle("Menu!");
         primaryStage.setScene(scene);
         primaryStage.show();
