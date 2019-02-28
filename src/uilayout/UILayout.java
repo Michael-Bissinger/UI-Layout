0c828da516346d0f32df903572b414bcae255020
Michael Bissinger <Michael.Bissinger@inb.de>
2019-02-28 11:11:53 +0100

    Create and add the ListView

--- src/uilayout/UILayout.java
+++ src/uilayout/UILayout.java
@@ -11,8 +11,8 @@ import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.FlowPane;
-import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -28,7 +28,8 @@ public class UILayout extends Application {
     @Override
     public void start(Stage primaryStage) {
         Label response = new Label("Select a college or university:");
-       
+
+        ListView<String> lvColleges;
        
         Text title = new Text("PA Colleges and Universities");
         title.setFill(Paint.valueOf("red"));
@@ -43,10 +44,14 @@ public class UILayout extends Application {
         ObservableList<String> colleges =
                 FXCollections.observableArrayList("a", "b", "c", "d", "e", "f", "g");
         
+        lvColleges = new ListView<String>(colleges);
+        lvColleges.setPrefSize(300, 150);
+        
         root.getChildren().add(title);
+        root.getChildren().add(lvColleges);
         root.getChildren().add(response);
         
-        Scene scene = new Scene(root, 400, 400, Color.WHITE);
+        Scene scene = new Scene(root, 350, 300);
         
        
         primaryStage.setTitle("Observable List!");
