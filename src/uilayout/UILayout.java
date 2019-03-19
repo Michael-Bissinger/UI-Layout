c3e00704fc28a4228581d0337e113003a8fdda1f
Michael Bissinger <Michael.Bissinger@inb.de>
2019-03-19 12:42:12 +0100

    Add piechart

--- src/uilayout/UILayout.java
+++ src/uilayout/UILayout.java
@@ -21,6 +21,20 @@ public class UILayout extends Application {
     
     @Override
     public void start(Stage primaryStage) {
+ 
+                ObservableList<PieChart.Data> pieData =
+                FXCollections.observableArrayList(
+                new PieChart.Data("Rent", 500),
+                        new PieChart.Data("Electric",125),
+                        new PieChart.Data("Groceries", 235),
+                        new PieChart.Data("Entertainment", 200),
+                        new PieChart.Data("Cell Phone", 80));
+        
+        PieChart budget = new PieChart(pieData);
+        budget.setTitle("Monthy expenses");
+        
+        FlowPane root = new FlowPane();
+        root.getChildren().add(budget);
         
         Scene scene = new Scene(root, 500, 500);
         
